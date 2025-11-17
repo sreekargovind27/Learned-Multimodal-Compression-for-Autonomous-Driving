b6fcc6be73935a7330d0c9589c1904645aee22bb
sreekargovind27 <govindsreekar27@gmail.com>
2025-11-17 12:36:37 -0500

    config file in general

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
-NUSCENES_BASE_PATH = "/path/to/nuscenes_data"
+NUSCENES_BASE_PATH = "/home/sreekarsArch/Documents/MultiModal_Data_Compression/nuscenes_data"
 MINI_PATH = NUSCENES_BASE_PATH + "/mini"
 CAN_BUS_PATH = NUSCENES_BASE_PATH + "/can_bus"
 MAPS_PATH = NUSCENES_BASE_PATH + "/maps"
 VERSION = "v1.0-mini"
-OUTPUT_DIR = "./output"
+OUTPUT_DIR = "/home/sreekarsArch/Documents/MultiModal_Data_Compression/output"
 RANDOM_SEED = 42
